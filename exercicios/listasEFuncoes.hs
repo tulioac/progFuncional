89663ae12563249e4f8d595485d948e077fc53d0
Túlio Araújo Cunha <tutex1500@gmail.com>
2022-05-21 16:31:17 -0300

    Implement more functions

--- exercicios/listasEFuncoes.hs
+++ exercicios/listasEFuncoes.hs
@@ -27,49 +27,52 @@ isPalindrome xs
 - Você pode usar a função elem de Haskell
 -}
 compress [] = []
-compress xs = compress (init xs) ++ [y | y <- [last xs], not y `elem` init xs]
+compress xs = head xs : compress (filter (/= head xs) (tail xs))
 
--- CONTINUAR DAQUI
 {-
 - Varre a lista da esquerda para a direita e junta os elementos iguais. Ex: compact [2,5,8,2,1,8] = [2,2,5,8,8,1]
 - Você pode usar funções sobre listas como : (cons), filter, etc.
 -}
--- for each element, create a list
-compact xs = undefined
+compact [] = []
+compact xs = head xs : filter (== head xs) (tail xs) ++ compact (filter (/= head xs) (tail xs))
 
 {-
 - Retorna uma lista de pares com os elementos e suas quantidades. Ex: encode [2,2,2,3,4,2,5,2,4,5] = [(2,5),(3,1),(4,2),(5,2)]
 - Você pode usar funções sobre listas como : (cons), filter, etc.
 -}
-encode xs = undefined
+encode [] = []
+encode xs = (head xs, length (filter (== head xs) xs)) : encode (filter (/= head xs) (tail xs))
 
 {-
-- Divide uma lista em duas sublistas onde o ponto de divisão é dado. Ex: split [3,6,1,9,4] 3 = [[3,6,1],[9,4]]
+- Divide uma lista em duas sub-listas onde o ponto de divisão é dado. Ex: split [3,6,1,9,4] 3 = [[3,6,1],[9,4]]
 -}
-split xs i = undefined
+split xs i = take i xs : [drop i xs]
 
 {-
 - Extrai um pedaço (slice) de uma lista especificado por um intervalo.
 - Ex: slice [3,6,1,9,4] 2 4 = [6,1,9]
 -}
-slice xs imin imax = undefined
+slice xs iMin iMax = drop (iMin - 1) (take iMax xs)
 
 {-
 - Insere um elemento em uma posição especifica de uma lista.
 - Ex: insertAt 7 4 [3,6,1,9,4] = [3,6,1,7,9,4]
 -}
-insertAt el pos xs = undefined
+insertAt el 1 xs = el : xs
+insertAt el pos xs = head xs : insertAt el (pos - 1) (tail xs)
 
 {-
-- Ordena uma lista em ordem crescente. Você deve seguir a ideia do selectionsort onde os elementos
-- menores sao trazidos para o inicio da lista um a um. Esta função ja esta implementada.
+- Ordena uma lista em ordem crescente. Você deve seguir a ideia do Selection Sort onde os elementos
+- menores são trazidos para o início da lista um a um. Esta função já está implementada.
 -}
+minList [] = undefined
 minList [x] = x
-minList (x : xs) = if (x < (minList xs)) then x else minList xs
+minList (x : xs) = if x < minList xs then x else minList xs
 
+remove e [] = []
 remove e (x : xs)
   | e == x = xs
-  | otherwise = x : (remove e xs)
+  | otherwise = x : remove e xs
 
 sort [] = []
 sort xs = x : ys
@@ -81,18 +84,25 @@ sort xs = x : ys
 - Dada a função max que retorna o máximo entre dois números, escreva uma função que usa a função
 - foldr e max para retornar o máximo de uma lista se a lista não é vazia.
 -}
-maxList xs = undefined
+maxList [] = undefined
+maxList xs = foldr max (head xs) xs
 
 {-
-- Transforma uma string em uma palindrome acrescentando o reverso da string ao seu final sem usar a função reverse.
+- Transforma uma string em um palíndromo acrescentando o reverso da string ao seu final sem usar a função reverse.
 - Ex: buildPalindrome [1,2,3] = [1,2,3,3,2,1].
 -}
-buildPalindrome xs = undefined
+buildPalindrome [] = []
+buildPalindrome xs = [head xs] ++ buildPalindrome (tail xs) ++ [head xs]
 
 {-
-- Computa a media dos elementos de uma lista de números, sem usar nenhuma função pronta de listas.
+- Computa a média dos elementos de uma lista de números, sem usar nenhuma função pronta de listas.
 -}
-mean xs = undefined
+mean xs = sumElements xs / fromIntegral (myLength xs)
+
+sumElements xs = foldr (+) 0 xs
+
+myLength [] = 0
+myLength xs = 1 + myLength (tail xs)
 
 {-
 - Escreva a função myAppend que faz o append de uma lista xs com a lista ys, usando a função foldr.
@@ -105,12 +115,12 @@ myAppend xs ys = undefined
 find p xs = undefined
 
 {-
-- Implementar quick sort usando compreensão de listas
+- Implementar Quick Sort usando compreensão de listas
 -}
 quickSort xs = undefined
 
 {-
-- Implementar quick sort usando filter
+- Implementar Quick Sort usando filter
 -}
 quickSortFilter xs = undefined
 
@@ -134,7 +144,7 @@ count x xs = undefined
 - pares contendo um elemento da lista original e sua quantidade de ocorrências
 - Cada elemento da lista original possui apenas um par correspondente na lista de resposta
 -}
-frequencia xs = undefined
+frequência xs = undefined
 
 {-
 - Função concat definida usando foldr
